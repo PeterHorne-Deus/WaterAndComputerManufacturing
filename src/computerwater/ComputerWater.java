372301dce92cb75c641c9ad13c99b9197c054594
pehor9164 <pehor9164@7KWN6Y1.Student.UGDSB.ED>
2018-10-02 09:03:23 -0400

    Finished project

---
+++ src/computerwater/ComputerWater.java
@@ -0,0 +1,44 @@
+/*
+ * Peter Horne-Deus 
+ * ComputerWater.java
+ * This program allows the user to input how many computers and monitors they have to calculate the amount of water used to make them
+ * October 2,2018
+ */
+
+package computerwater;
+
+import java.util.Scanner;
+
+public class ComputerWater {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Creating Scanner
+        Scanner keyedInput = new Scanner(System.in);
+        
+        //Variables
+        double waterUsedMonitors,waterUsedComputers,totalUsed;
+        int numComputers,numMonitors;
+        
+        //user Input
+        System.out.println("Enter the number of Computers you own");
+        numComputers = keyedInput.nextInt();
+        
+        System.out.println("Enter the number of monitors you own");
+        numMonitors = keyedInput.nextInt();
+        
+        //Caculations
+        waterUsedMonitors = numMonitors * 0.5;
+        waterUsedComputers = numComputers * 1;
+        totalUsed = waterUsedComputers + waterUsedMonitors; 
+        
+        //Output
+        System.out.println("The total water used for you monitors and computers is " + totalUsed + " Tons");
+        
+        //Ending program
+        keyedInput.close();
+    }
+    
+}
