15645f65e53aedd94aafbb3dee55a5f375ae79ff
pehor9164 <pehor9164@7KWN6Y1.Student.UGDSB.ED>
2018-10-02 09:41:17 -0400

    update 1.1 including constants

--- src/computerwater/ComputerWater.java
+++ src/computerwater/ComputerWater.java
@@ -19,6 +19,8 @@ public class ComputerWater {
         Scanner keyedInput = new Scanner(System.in);
         
         //Variables
+        final double monitorWater = 0.5;
+        final double computerWater = 1.0;
         double waterUsedMonitors,waterUsedComputers,totalUsed;
         int numComputers,numMonitors;
         
@@ -30,8 +32,8 @@ public class ComputerWater {
         numMonitors = keyedInput.nextInt();
         
         //Caculations
-        waterUsedMonitors = numMonitors * 0.5;
-        waterUsedComputers = numComputers * 1;
+        waterUsedMonitors = numMonitors * monitorWater;
+        waterUsedComputers = numComputers * computerWater;
         totalUsed = waterUsedComputers + waterUsedMonitors; 
         
         //Output
